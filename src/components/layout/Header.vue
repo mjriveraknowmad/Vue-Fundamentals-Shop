12ab26e1ef2e52e486c79f46349f141df9d02b14
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-26 09:52:36 +0200

    feat: Componente Header con el navegación usando el enrutado y cambios de Tema. Creación del Store para manejar el tema oscuro / claro

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,1 +1,96 @@
-<template>Header</template>
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <img src="../../assets/logo.png" style="width: 40px" class="mx-3" alt="ShopOPedia" />
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              :to="{ name: APP_ROUTE_NAMES.HOME }"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              :to="{ name: APP_ROUTE_NAMES.PRODUCT_LIST }"
+              >Product</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              :to="{ name: APP_ROUTE_NAMES.PRODUCT_CREATE }"
+              >Add Product</router-link
+            >
+          </li>
+        </ul>
+
+        <ul class="d-flex navbar-nav">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i class="bi bi-laptop"></i>
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <button @click="themeStore.setTheme('light')" class="dropdown-item">
+                  <i class="bi bi-sun"></i> &nbsp; Light
+                </button>
+              </li>
+              <li>
+                <button class="dropdown-item" @click="themeStore.setTheme('dark')">
+                  <i class="bi bi-moon-stars-fill"></i> &nbsp; Dark
+                </button>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              :to="{ name: APP_ROUTE_NAMES.SIGN_IN }"
+              >Sign In</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              :to="{ name: APP_ROUTE_NAMES.SIGN_UP }"
+              >Sign Up</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { APP_ROUTE_NAMES } from '@/constants/routeNames'
+import { useRouter } from 'vue-router'
+import { useThemeStore } from '@/stores/themeStore'
+const themeStore = useThemeStore()
+const router = useRouter()
+</script>
